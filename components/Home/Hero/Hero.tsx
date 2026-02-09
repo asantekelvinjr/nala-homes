089edef2e5525eef8ede9691f8b081416aee8d32
El Mago <asantekelvinjr@gmail.com>
2026-02-09 13:11:47 +0000

    Hero alignment made on mobile

--- components/Home/Hero/Hero.tsx
+++ components/Home/Hero/Hero.tsx
@@ -15,7 +15,7 @@ const Hero = () => {
       <div className="absolute inset-0 bg-black/50"></div>
 
       {/* Content */}
-      <div className="flex flex-col gap-2  justify-center relative z-10 mx-auto">
+      <div className="w-[90%] flex flex-col gap-2  justify-center relative z-10 mx-auto">
         {/* Heading */}
         <div>
           <h1 className="text-white  text-3xl sm:text-4xl md:text-5xl lg:text-[52px] xl:text-[52px] 2xl:text-[52px] leading-16 max-w-[600px] text-center lg:text-left  lg:max-w-[690px] font-semibold">
@@ -30,22 +30,22 @@ const Hero = () => {
           </p>
         </div>
 
-<div className="flex justify-center md:justify-start">
-  <div className="relative w-full max-w-md mt-5">
-    <FaSearch
-      className="
+        <div className="flex justify-center md:justify-start">
+          <div className="relative w-full max-w-md mt-5">
+            <FaSearch
+              className="
         absolute
         left-5 sm:left-12 md:left-4
         top-1/2
         -translate-y-1/2
         text-gray-400
       "
-    />
+            />
 
-    <input
-      type="text"
-      placeholder="Search By Location ..."
-      className="
+            <input
+              type="text"
+              placeholder="Search By Location ..."
+              className="
         bg-white text-[#928D8D]
         border
         py-4
@@ -54,10 +54,10 @@ const Hero = () => {
         rounded-full
         w-full
       "
-    />
+            />
 
-    <button
-      className="
+            <button
+              className="
         absolute
         right-4
         top-1/2
@@ -68,12 +68,11 @@ const Hero = () => {
         flex items-center justify-center
         hover:bg-blue-600
       "
-    >
-      <FaArrowRight className="w-5 h-5" />
-    </button>
-  </div>
-</div>
-
+            >
+              <FaArrowRight className="w-5 h-5" />
+            </button>
+          </div>
+        </div>
       </div>
     </section>
   );
